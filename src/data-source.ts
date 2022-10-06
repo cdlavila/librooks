7ea5a0783a739edd038d09838d55f74975a7d882
Carlos <carlosdaniel.londono@utp.edu.co>
2022-10-06 15:18:19 -0500

    data-source okay

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,15 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'librooks-admin',
-  password: 'librooks-mqW6ZzKnat',
-  database: 'librooks',
-  // host: process.env.DATABASE_HOST,
-  // port: parseInt(process.env.DATABASE_PORT, 10),
-  // name: process.env.DATABASE_NAME,
-  // username: process.env.DATABASE_USER,
-  // password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   synchronize: false,
   logging: true,
   entities: ['src/**/*.entity.ts'],
