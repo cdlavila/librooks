f53bd8d25889b9ed782d23a92b78a52606776333
Juan_Montoya <montoyazanty@gmail.com>
2022-10-11 21:16:27 -0500

    crud admin

--- src/users/services/admins.service.ts
+++ src/users/services/admins.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Admin } from '../entities/admin.entity';
+import { Repository } from 'typeorm';
+import { CreateAdminDto } from '../dtos/admins.dtos';
+import { CreateUserDto } from '../dtos/users.dtos';
 
 @Injectable()
-export class AdminsService {}
+export class AdminsService {
+  constructor(
+    @InjectRepository(Admin) private adminsRepository: Repository<Admin>,
+  ) {}
+
+  create(data: CreateAdminDto) {
+    const newAdmin = this.adminsRepository.create(data);
+    return this.adminsRepository.save(newAdmin);
+  }
+
+  async findOne(id: string) {
+    const admin = await this.adminsRepository.findOne({ where: { id } });
+    if (!admin) {
+      throw new NotFoundException(`Administrador ${id} no encontrado`);
+    }
+    return admin;
+  }
+
+  async find() {
+    const admins = await this.adminsRepository.find();
+    return admins;
+  }
+
+  async update(id: string, changes: CreateAdminDto) {
+    const admin = await this.adminsRepository.findOne({ where: { id } });
+    if (!admin) {
+      throw new NotFoundException(`Administrador ${id} no encontrado`);
+    }
+    this.adminsRepository.merge(admin, changes);
+    return this.adminsRepository.save(admin);
+  }
+
+  async delete(id: string) {
+    const admin = await this.adminsRepository.findOne({ where: { id } });
+    if (!admin) {
+      throw new NotFoundException(`Administrador ${id} no encontrado`);
+    }
+    return this.adminsRepository.delete(id);
+  }
+}
