25f86cb19d7d26b6994d8e8a3b0da6498deb91ab
Carlos <carlosdaniel.londono@utp.edu.co>
2022-11-02 12:16:07 -0500

    Fix global error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+// import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: '.env' });
@@ -9,13 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.enableCors();
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     // forbidNonWhitelisted: true,
+  //     // transform: true,
+  //   }),
+  // );
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap().then(() => {
