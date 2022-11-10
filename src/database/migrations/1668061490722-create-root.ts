1e5a0f8fe8f37cce6e1d1830be9d1f183581e15b
Carlos <carlosdaniel.londono@utp.edu.co>
2022-11-10 01:32:44 -0500

    Create root as a migration

---
+++ src/database/migrations/1668061490722-create-root.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as dotenv from 'dotenv';
+import * as bcrypt from 'bcrypt';
+
+dotenv.config({ path: '.env' });
+
+export class createRoot1668061490722 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.insert('users', {
+      id: '8d4169b9-673b-4819-8097-376548c51a15',
+      username: 'root',
+      email: 'carlosdaniel.londono@utp.edu.co',
+      password: bcrypt.hashSync(process.env.ROOT_PASSWORD, 10),
+      role: 'root',
+      is_active: 1,
+    });
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.manager.delete('users', {
+      id: '8d4169b9-673b-4819-8097-376548c51a15',
+    });
+  }
+}
