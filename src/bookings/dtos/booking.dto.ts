0e1691a719a82a3b784b97a33189d63df14b7443
Juan_Montoya <montoyazanty@gmail.com>
2022-11-15 22:05:24 -0500

    Validations DTOS

--- src/bookings/dtos/booking.dto.ts
+++ src/bookings/dtos/booking.dto.ts
@@ -1,18 +1,36 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { Client } from '../../users/entities/client.entity';
 import { Store } from '../../books/entities/store.entity';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
 
 export class CreateBookingDto {
+  @IsString({ message: 'Los detalles deben ser un texto' })
+  @IsNotEmpty({ message: 'Los detalles son requeridos' })
   readonly details: string;
 
+  @IsString({ message: 'El estado debe ser un texto' })
+  @IsNotEmpty({ message: 'El estado es requerido' })
   readonly status: string;
 
+  @IsDate({ message: 'Debe ser una fecha' })
+  @IsNotEmpty({ message: 'La fecha es requerida' })
   readonly date: Date;
 
+  @IsPositive({ message: 'El total debe ser un número positivo' })
+  @IsNumber({}, { message: 'El total debe ser un número' })
+  @IsNotEmpty({ message: 'El total es requerido' })
   readonly total: number;
 
+  @IsNotEmpty({ message: 'El cliente es requerido' })
   readonly client: Client;
 
+  @IsNotEmpty({ message: 'La tienda es requerida' })
   readonly store: Store;
 }
 
--- src/purchases/dtos/purchase.dto.ts
+++ src/purchases/dtos/purchase.dto.ts
@@ -1,16 +1,30 @@
 import { Client } from '../../users/entities/client.entity';
 import { Store } from '../../books/entities/store.entity';
 import { PartialType } from '@nestjs/mapped-types';
+import { IsDate, IsIn, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreatePurchaseDto {
+  @IsString({ message: 'El campo "detalles" debe ser de tipo texto' })
+  @IsNotEmpty({ message: 'El campo "detalles" no puede estar vacío' })
   readonly details: string;
 
+  @IsString({ message: 'El campo "estado" debe ser de tipo texto' })
+  @IsNotEmpty({ message: 'El campo "estado" no puede estar vacío' })
   readonly status: string;
 
+  @IsDate({ message: 'El campo "fecha" debe ser de tipo fecha' })
+  @IsNotEmpty({ message: 'El campo "fecha" no puede estar vacío' })
   readonly date: Date;
 
+  @IsNumber({}, { message: 'El campo "total" debe ser de tipo numérico' })
+  @IsNotEmpty({ message: 'El campo "total" no puede estar vacío' })
   readonly total: number;
 
+  @IsIn(['Recogida en tienda', 'Envío a domicilio'], {
+    message:
+      'El campo "método de entrega" debe ser "Recogida en tienda" o "Envío a domicilio"',
+  })
+  @IsNotEmpty({ message: 'El campo "método de entrega" no puede estar vacío' })
   readonly deliveryMethod: 'Recogida en tienda' | 'Envío a domicilio';
 
   readonly client: Client;
